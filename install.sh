d6cf20296e64214c62876f9afeab6277a8e49741
ylwnet <ylwnet@sina.com>
2020-01-20 07:15:18 +0800

    修改：      install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,8 @@ echo "
 +----------------------------------------------------------------------
 "
 get_node_url(){
-	nodes=(http://149.129.95.118:5522 http://103.224.251.67 http://128.1.164.196 http://download.bt.cn);
+	nodes=(http://149.129.95.118:5522);
+         #nodes=(http://149.129.95.118:5522 http://103.224.251.67 http://128.1.164.196 http://download.bt.cn);
 	i=1;
 	if [ ! -f /bin/curl ];then
 		if [ -f /usr/local/curl/bin/curl ];then
